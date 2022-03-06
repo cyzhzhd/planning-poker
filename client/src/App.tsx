071d6c6ed955f1889c5f2072bc8af6065704097e
cyzhzhd <cyzhzhd@gmail.com>
2022-03-06 17:20:20 +0900

    client:이모션(css) 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Global, ThemeProvider } from '@emotion/react';
 import Home from './presenters/Home';
 import Login from './presenters/Login';
 import PokerRoom from './presenters/PokerRoom';
 import PokerProvider from './states/poker/PokerProvider';
 import UserProvider from './states/user/UserProvider';
+import { globalStyles } from './styles/global';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <PokerProvider>
-        <UserProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="poker-room" element={<PokerRoom />} />
-          </Routes>
-        </UserProvider>
-      </PokerProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={{}}>
+      <Global styles={globalStyles} />
+      <BrowserRouter>
+        <PokerProvider>
+          <UserProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="poker-room" element={<PokerRoom />} />
+            </Routes>
+          </UserProvider>
+        </PokerProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
