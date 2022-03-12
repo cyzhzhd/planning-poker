f48701845c54092a45e2f14cf42adf863c9d75a5
cyzhzhd <cyzhzhd@gmail.com>
2022-03-12 13:39:40 +0900

    temp:change port

--- client/src/helpers/client.ts
+++ client/src/helpers/client.ts
@@ -1,6 +1,6 @@
 import { PokerServiceClient } from '../proto/PokerServiceClientPb';
 
-const client = new PokerServiceClient('http://localhost:30009');
+const client = new PokerServiceClient('http://192.168.219.105:30009');
 console.log('client created', client);
 
 export default client;
