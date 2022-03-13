bf34c5ce1f57680ae3a96a7e136eead0b17b42a6
Ubuntu <ubuntu@ip-172-31-37-36.ap-northeast-2.compute.internal>
2022-03-13 08:06:19 +0000

    temp: change port

--- client/src/helpers/client.ts
+++ client/src/helpers/client.ts
@@ -1,6 +1,6 @@
 import { PokerServiceClient } from '../proto/PokerServiceClientPb';
 
-const client = new PokerServiceClient('http://15.164.224.87:8080');
+const client = new PokerServiceClient('http://15.164.224.87:30009');
 console.log('client created', client);
 
 export default client;
